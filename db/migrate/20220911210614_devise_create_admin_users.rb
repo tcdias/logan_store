4dbca6d425afc41fe470ce43199fa70eb81ad776
Thomas Dias <tcdias@gmail.com>
2022-09-11 20:27:12 -0300

    "add listing model and admin"

--- db/migrate/20220911210614_devise_create_admin_users.rb
+++ db/migrate/20220911210614_devise_create_admin_users.rb
@@ -2,7 +2,6 @@
 
 class DeviseCreateAdminUsers < ActiveRecord::Migration[7.0]
   def change
-    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
     create_table :admin_users, id: :uuid, default: 'gen_random_uuid()' do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
